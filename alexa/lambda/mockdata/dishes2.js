a28c63caa1ee1cb32e0eb48c370416930e9e08e5
Theo_o <bernd.eichhorn2@web.de>
2021-06-28 16:56:08 +0200

    alexa tests

---
+++ alexa/lambda/mockdata/dishes2.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    {
+      dish: {
+        name: "Thai Spicy Basil Fried Rice",
+      },
+    },
+    {
+      dish: {
+        name: "Beans",
+      },
+    },
+    {
+      dish: {
+        name: "Salad",
+      }
+    }
+  ]
+};
--- alexa/lambda/utilMock.js
+++ alexa/lambda/utilMock.js
@@ -1,7 +1,36 @@
+
 const AWS = require("aws-sdk");
 const config = require("./config");
 const http = require("http");
 
+const dishes = require("./mockdata/dishes");
+
 module.exports.createReservation = (name, email) => {
+  return Promise.resolve(()=>{})
+};
+
+module.exports.getTableByDeviceId = (deviceId) => {
+  return Promise.resolve(()=>{})
+};
+
+module.exports.addOrderInhouse = (orderlines, deviceId) => {
+  return Promise.resolve(()=>{})
+};
+
+
+module.exports.getTableByDeviceId = (deviceId) => {
+  return Promise.resolve(()=>{})
+};
+
+
+module.exports.getDishes = (size, number) => {
+  return Promise.resolve(()=>dishes)
+};
+
+module.exports.getTableByDeviceId = (deviceId) => {
+  return Promise.resolve(()=>{})
+};
+
+module.exports.getTableByDeviceId = (deviceId) => {
   return Promise.resolve(()=>{})
 };
